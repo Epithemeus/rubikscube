2365dd1cdca353ca6d48e8b4e62a0f9f97eccd47
epithemeus <banemalar7@hotmail.com>
2016-04-10 08:38:55 +0000

    Tidyed up backwards/forwards behaviour. Added double moves

--- app/assets/javascripts/OrbitControls.js
+++ app/assets/javascripts/OrbitControls.js
@@ -27,11 +27,11 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 	// How far you can dolly in and out ( PerspectiveCamera only )
 	this.minDistance =2.82842712475;
-	this.maxDistance = Infinity;
+	this.maxDistance = 15;
 
 	// How far you can zoom in and out ( OrthographicCamera only )
 	this.minZoom = 0;
-	this.maxZoom = Infinity;
+	this.maxZoom = 8;
 
 	// How far you can orbit vertically, upper and lower limits.
 	// Range is 0 to Math.PI radians.
